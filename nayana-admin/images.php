de5ca85dc3f2c06e8b3670eda4a21bdd7b97d04b
Uday Veduruvada <uday.veduruvada@thegisolutions.com>
2023-11-03 22:53:17 +0530

    categories added

---
+++ nayana-admin/images.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+
+<?php include_once('./head.php'); ?>
+    <div class="container-fluid" >
+        <div class="row">
+            <div class="col-md-2">
+                <?php include_once('./sidebar.php'); ?>
+            </div>
+            <div class="col-md-10">
+                <h4 class="pt-3">Images</h4>
+            </div>
+        </div>
+    </div>
+<?php include_once('./foot.php'); ?>
--- nayana-admin/index.php
+++ nayana-admin/index.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the username and password are not empty
     if (!empty($_POST['username']) && !empty($_POST['password'])) {
@@ -11,6 +12,7 @@
           if ($password == '123456') {
             // Redirect to the dashboard
             header("Location: dashboard.php");
+            $_SESSION['authenticated'] = true;
             exit();
           } else {
             // Set error message in session
@@ -27,18 +29,7 @@
   }
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
+<?php include_once('./head.php'); ?>
     <div class="container">
         <div class="row mt-5 pt-5">
             <div class="col-md-6 offset-md-3">
@@ -74,10 +65,5 @@
             </div>
         </div>
     </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php include_once('./foot.php'); ?>
+    
